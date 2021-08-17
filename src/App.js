fd746d11411e85b18a562f8f185879b1a6e477a4
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-08-17 22:59:48 +0200

    start with Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='px-6 py-8 is-justify-content-left align-items-center'>
+        <h1 className='text-center text-4xl text-white-600 font-bold'>Hello world</h1>
+
+        <p className='text-left'>logo</p>
+      </div>
     </div>
   );
 }
