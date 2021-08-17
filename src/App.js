0492d2ffc26260cbd4c85b17a1ef8588118acec6
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-08-17 23:40:33 +0200

    add more classes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 function App() {
   return (
-    <div>
-      <div className='px-6 py-8 is-justify-content-left align-items-center'>
-        <h1 className='text-center text-4xl text-white-600 font-bold'>Hello world</h1>
-
-        <p className='text-left'>logo</p>
+    <div className='container  mx-auto text-center mt-11 bg-yellow-500'>
+      <div className='grid grid-cols-3 gap-4 text-white text-xl'>
+        <div>Banka APP</div>
+        <div>Name: Bojan Mijatovic</div>
+        <div>Login</div>
+      </div>
+      <div className='grid grid-cols-4 gap-10 pt-9 text-center text-is-justify-content-center text-2xl text-white bg-gray-700'>
+        <div>Home</div>
+        <div>Products</div>
+        <div>Transactions</div>
+        <div>News</div>
+      </div>
+      <div className='flex flex-row px-6 mt-5 ml-14'>
+        <div className='text-2xl text-gray-900 bg-yellow-50 mx-6 m-4 text-center p-2'>
+          <p>account</p>
+        </div>
+        <div className='text-2xl text-gray-900 bg-yellow-50 mx-6 m-4 text-center p-2'>
+          <p>cards</p>
+        </div>
+        <div className='text-2xl text-gray-900 bg-yellow-50 mx-6 m-4 text-center p-2'>
+          <p>loans</p>
+        </div>
       </div>
     </div>
   );
